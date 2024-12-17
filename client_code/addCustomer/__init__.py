4b48981624a6d0efe7450ff26ef8009a3cfe6e29
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2024-12-17 22:32:41 +0000

    Created addPayment.RowTemplate1, customerManagement, addPayment and LICENSE.txt; Edited addCustomer, settings and home

--- client_code/addCustomer/__init__.py
+++ client_code/addCustomer/__init__.py
@@ -1,5 +1,10 @@
 from ._anvil_designer import addCustomerTemplate
 from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class addCustomer(addCustomerTemplate):
---
+++ server_code/customerManagement.py
@@ -0,0 +1,19 @@
+import anvil.secrets
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import stripe
+from stripe.error import StripeError
+
+stripe.api_key = 'your_secret_key'
+
+try:
+    customers = stripe.Customer.search(
+        query="name:'Jane Doe'"
+    )
+    for customer in customers.auto_paging_iter():
+        print(f"Customer ID: {customer.id}, Email: {customer.email}")
+except StripeError as e:
+    print(f"An error occurred: {e.user_message}")
